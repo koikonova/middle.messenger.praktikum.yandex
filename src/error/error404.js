c1744f9329024609f642795df57a6befa77eefb9
chelseajoint <91132438+chelseajoint@users.noreply.github.com>
2023-03-28 21:29:48 +0200

    add components

--- src/error/error404.js
+++
@@ -1,9 +0,0 @@
-import Handlebars from 'handlebars';
-import styles from "./style.scss";
-import errorTpl from './errorTpl';
-
-const error404 = () => {
-    return Handlebars.compile(errorTpl)({error: '404', title: 'Не туда попали'});
-};
-
-export default error404;
--- src/error/error500.js
+++
@@ -1,9 +0,0 @@
-import Handlebars from 'handlebars';
-import styles from "./style.scss";
-import errorTpl from './errorTpl';
-
-const error500 = () => {
-    return Handlebars.compile(errorTpl)({error: '500', title: 'Мы уже фиксим'});
-};
-
-export default error500;
